35fa5d1f1251a47ec13429b15c46e350b01c9597
Pablo Requena <pablorequena07@gmail.com>
2020-04-14 10:39:53 +0200

    -

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -11,9 +11,16 @@ export class InfoPaginaService {
   //cargada: boolean = false;
   cargada = false;
 
+  equipo: any[] = [];
+
   constructor( private http: HttpClient ) {
 
-    // console.log("Servicio de infoPagina listo");
+    this.cargarInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargarInfo() {
 
     // Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
@@ -24,9 +31,24 @@ export class InfoPaginaService {
 
       //console.log(resp);
       //console.log( resp['email'] );
-      console.log(resp);
+      //console.log(resp);
 
     });
 
   }
+
+  private cargarEquipo() {
+
+    // Leer el archivo JSON
+    this.http.get('https://angular-html-e9db6.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+
+      this.equipo = resp;
+
+      //console.log(resp);
+
+    });
+
+  }
+
 }
